b46173e392840893ebf72b80e24d486b5ae8d804
bilal iken <bilal11iken@gmail.com>
2025-11-14 00:21:34 +0100

    configuration de server et conexion de base des donnees

---
+++ server.js
@@ -0,0 +1,22 @@
+  import express from "express"
+  import dotenv from 'dotenv';
+  import {connectdb} from "./lib/db.js"
+ 
+
+
+  dotenv.config();
+
+
+  const app = express();
+
+  app.use(express.json());
+
+  const PORT = process.env.PORT
+
+
+
+
+  connectdb()
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
